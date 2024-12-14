4b4e8ea3309b4cd7dd20ebad9da4f42538c45557
Krille <kristian.risvaer@gmail.com>
2024-12-14 01:18:53 +0100

    refactoring

--- init.lua
+++ init.lua
@@ -1,3 +1,43 @@
+require("core.options")
+require("core.remap")
+require("core.snippets")
+-- require("krille.lazy")
+-- require 'core.options' -- Load general options
+-- require 'core.keymaps' -- Load general keymaps
+-- require 'core.snippets' -- Custom code snippets
 
-require("krille.core")
-require("krille.lazy")
+-- Set up the Lazy plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set up plugins
+require("lazy").setup({
+	require("plugins.lsp.lspconfig"),
+	require("plugins.lsp.mason"),
+	require("plugins.colorscheme"),
+	require("plugins.bufferline"),
+	require("plugins.comment"),
+	require("plugins.formatting"),
+	require("plugins.indent-blankline"),
+	require("plugins.linting"),
+	require("plugins.lualine"),
+	require("plugins.nvim-cmp"),
+	require("plugins.nvim-tree"),
+	require("plugins.telescope"),
+	require("plugins.treesitter"),
+	require("plugins.trouble"),
+	require("plugins.undotree"),
+	require("plugins.vim-maximizer"),
+	require("plugins.which-key"),
+	require("plugins.misc"),
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -5,6 +5,9 @@ local keymap = vim.keymap -- for conciseness
 -- not needed with nvim-tree
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x', opts)
+
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Able to move highlighted section" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Able to move highlighted section" })
 
@@ -26,7 +29,7 @@ keymap.set("i", "<C-c>", "<Esc>", { desc = "C-c works as <Esc> in insertmode" })
 -- keymap.set("n", "<leader>f", vim.lsp.buf.format)     crash with find files in telescope
 
 -- keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])    crash with split window!!!
-keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+keymap.set("n", "<leader>xx", "<cmd>!chmod +x %<CR>", { silent = true })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
@@ -36,8 +39,11 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- Buffers
+keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Go to next buffer", noremap = true, silent = true })
+keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Go to next buffer", noremap = true, silent = true })
+-- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+-- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+-- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+-- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+-- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
